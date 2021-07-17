e063548a5d9eece9a2a75cae609cf9b41cc80182
mathew-fleisch <mathew.fleisch@gmail.com>
2021-07-17 13:59:34 -0700

    fix bug referencing vpc id

--- modules/aws/vpc.tf
+++ modules/aws/vpc.tf
@@ -17,7 +17,7 @@ resource "aws_vpc" "vpc" {
 
 # Internet gateway for the public subnets
 resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id = "aws_vpc.vpc.id"
+  vpc_id = aws_vpc.vpc.id
 
   tags = {
     Name = "${var.cluster_name}-internet-gateway"
