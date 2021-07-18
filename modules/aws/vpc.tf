833e9d4508adda626b89cbded0e15e5df2ed06f8
mathew-fleisch <mathew.fleisch@gmail.com>
2021-07-18 15:29:13 -0700

    Enable dns support in vpc

--- modules/aws/vpc.tf
+++ modules/aws/vpc.tf
@@ -31,6 +31,7 @@ resource "aws_security_group" "ssh_all" {
 resource "aws_vpc" "vpc" {
   cidr_block           = "10.20.0.0/16"
   enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
     ClusterName = var.cluster.name
